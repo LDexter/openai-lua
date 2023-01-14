f2ee7caf83a3b630cb41f85d808446c641e48324
Levi Miller <ldextermiller@gmail.com>
2023-01-14 13:48:15 +1000

    Added model selection with handy options guide.

--- openai.lua
+++ openai.lua
@@ -1,8 +1,30 @@
 local openai = {}
 
 
--- Request text from OpenAI, given provided prompt, temperature, and maximum tokens
-function openai.request(prompt, temp, tokens)
+--[[
+?MODEL GUIDE (read more at https://beta.openai.com/docs/models)
+
+*text-davinci-003:
+Most capable GPT-3 model. Can do any task the other models can do,
+often with higher quality, longer output and better instruction-following.
+Also supports inserting completions within text.
+[Good at: Complex intent, cause and effect, summarization for audience]
+
+*text-curie-001:
+Very capable, but faster and lower cost than Davinci.
+[Good at: Language translation, complex classification, text sentiment, summarization]
+
+*text-babbage-001:
+Capable of straightforward tasks, very fast, and lower cost.
+[Good at: Moderate classification, semantic search classification]
+
+*text-ada-001:
+Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.
+[Good at: Parsing text, simple classification, address correction, keywords]
+]]
+
+-- Request completion from OpenAI, given provided model, prompt, temperature, and maximum tokens
+function openai.complete(model, prompt, temp, tokens)
     -- Accessing private key in local .env file
     local cmplEnv = fs.open("/DavinCC/.env", "r")
     local cmplAuth = cmplEnv.readAll()
@@ -10,7 +32,7 @@ function openai.request(prompt, temp, tokens)
 
     -- Posting to OpenAI using the private key
     local cmplPost = http.post("https://api.openai.com/v1/completions",
-        '{"model": "text-davinci-003", "prompt": "' .. prompt .. '", "temperature": ' .. temp .. ', "max_tokens": ' .. tokens .. '}',
+        '{"model": "' .. model .. '", "prompt": "' .. prompt .. '", "temperature": ' .. temp .. ', "max_tokens": ' .. tokens .. '}',
         { ["Content-Type"] = "application/json", ["Authorization"] = "Bearer " .. cmplAuth })
     return cmplPost.readAll()
 end
