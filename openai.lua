e0e1edeba5407fef427046bf7d2294c60ff085f4
Levi Miller <ldextermiller@gmail.com>
2023-03-02 16:36:39 +1000

    Updated model guide commenting

--- openai.lua
+++ openai.lua
@@ -9,21 +9,19 @@ openai.isFlagged = false
 --[[
 ?MODEL GUIDE (read more at https://beta.openai.com/docs/models)
 
-*text-davinci-003:
-Most capable GPT-3 model. Can do any task the other models can do,
-often with higher quality, longer output and better instruction-following.
-Also supports inserting completions within text.
+*davinci:
+Most capable GPT-3 model. Can do any task the other models can do, often with higher quality.
 [Good at: Complex intent, cause and effect, summarization for audience]
 
-*text-curie-001:
+*curie:
 Very capable, but faster and lower cost than Davinci.
 [Good at: Language translation, complex classification, text sentiment, summarization]
 
-*text-babbage-001:
+*babbage:
 Capable of straightforward tasks, very fast, and lower cost.
 [Good at: Moderate classification, semantic search classification]
 
-*text-ada-001:
+*ada:
 Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.
 [Good at: Parsing text, simple classification, address correction, keywords]
 ]]
